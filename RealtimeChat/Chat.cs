dbf7cad48893062366c3b98850c01d5a3d915f46
Ville Salonen <ville.salonen@iki.fi>
2013-04-05 17:53:51 +0300

    Imported first version from SignalR hub tutorial.

---
+++ RealtimeChat/Chat.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace RealtimeChat
+{
+    public class Chat : Hub
+    {
+        public void Send(string message)
+        {
+            // Call the addMessage method on all clients            
+            Clients.All.addMessage(message);
+        }
+    }
+}
---
+++ RealtimeChat/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RealtimeChat.Global" Language="C#" %>
